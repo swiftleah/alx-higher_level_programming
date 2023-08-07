6542627e9727bcdf0634b757e8c0bfd81cee3f47
swiftleah <leahrosehenry@gmail.com>
2023-08-07 10:20:36 -0700

    Reduced characters task 9

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-file_path = 'poem.txt'
-with open(file_path, 'r') as file:
-    for line in file:
-        print(line.strip())
+with open('poem.txt', 'r') as f:
+    for x in f:
+        print(x.strip())
