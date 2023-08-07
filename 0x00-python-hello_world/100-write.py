15fb72b03b6068df05b6dad22f8eb701f5542c83
swiftleah <leahrosehenry@gmail.com>
2023-08-07 03:49:28 -0700

    Changed newline location

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,6 +2,6 @@
 
 import sys
 
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(message + "\n")
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(message)
 sys.exit(1)
