a14513e11873da3e19fed8fff79b9cc9c6b0ca36
swiftleah <leahrosehenry@gmail.com>
2023-08-08 06:00:34 -0700

    Task 3 completed

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+for char in 'abcdefghijlmnopqrstuvxyz':
+    if char != 'q' and char != 'e':
+        print(char, end='')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    now_upp = ''.join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in str)
+    now_upp = str.upper()
     print("{}".format(now_upp))
