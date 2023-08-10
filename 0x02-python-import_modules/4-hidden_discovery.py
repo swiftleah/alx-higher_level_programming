38015b299b7575d78f3aa3cf1ca468807c6ef2a0
swiftleah <leahrosehenry@gmail.com>
2023-08-10 09:37:34 -0700

    Fixed code style requirement for task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    hidden_names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    for name in hidden_names:
+    hid_names = [name for name in dir(hidden_4) if not name.startswith('__')]
+    for name in hid_names:
         print("{}".format(name))
