6666706a718c3764b1931d674d7268276466d18e
swiftleah <leahrosehenry@gmail.com>
2023-08-10 09:44:23 -0700

    Fixed task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
-        print("1: {}".format(argv[0]))
     else:
         print("{} arguments:".format(argc))
     for i, arg in enumerate(argv, start=1):
