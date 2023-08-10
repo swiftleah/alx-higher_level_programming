9be34320ec7dc1b36967085943e9eafef68781cd
swiftleah <leahrosehenry@gmail.com>
2023-08-10 01:38:47 -0700

    Task 9 completed - need to change program bc too many lines

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, result))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import string, sys
+sys.stdout.write(string.ascii_uppercase + '\n')
