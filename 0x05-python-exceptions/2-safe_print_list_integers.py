5638a49723098cc7f6021417211b92a8dbafa1eb
swiftleah <leahrosehenry@gmail.com>
2023-08-28 06:23:28 -0700

    Changed in range condition task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         except (IndexError, ValueError):
-            continue
+            break
     print()
     return count
