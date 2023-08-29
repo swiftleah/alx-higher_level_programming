9d7b0fd26180c522c20415deaa3643f5989dd687
swiftleah <leahrosehenry@gmail.com>
2023-08-29 03:14:37 -0700

    Added documentation for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Documentation:
+    Class 'Square' that defines a square with private instance attribute 'size'
+"""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
