6b7035ba956b92c9e37ae3106eeb4bbc343b6fe9
swiftleah <leahrosehenry@gmail.com>
2023-08-29 05:53:12 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" defines class 'Square'"""
+
+
+class Square:
+    """ class 'Square'"""
+    def __init__(self, size):
+        """ Initializes class 'Square'
+        Args: size of square in ints
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.size)
