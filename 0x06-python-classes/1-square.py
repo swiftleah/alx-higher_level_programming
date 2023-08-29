b36bb39c87720e06516f6c811b5cd5db0e1885f8
swiftleah <leahrosehenry@gmail.com>
2023-08-29 03:25:45 -0700

    Added more documentation for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,10 @@
 
 
 class Square:
+    """ class 'Square'"""
     def __init__(self, size):
+        """ Initialize new 'Square'
+        Args:
+        size - private attribute for size of square
+        """
         self.__size = size
