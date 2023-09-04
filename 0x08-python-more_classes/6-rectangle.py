a8d7c3a45fb0bf5e77b12763d9aa9c30ea62c8cd
swiftleah <leahrosehenry@gmail.com>
2023-09-04 06:09:43 -0700

    Task 6 attempt 2

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """
         self.__width = width
         self.__height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -50,4 +50,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
