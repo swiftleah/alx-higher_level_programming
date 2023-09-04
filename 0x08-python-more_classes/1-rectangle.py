dc7b0bee53897354459d02c59f8a3a2c83afcb33
swiftleah <leahrosehenry@gmail.com>
2023-09-04 01:46:09 -0700

    Fixed pycodestyle errors for task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,6 +26,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
