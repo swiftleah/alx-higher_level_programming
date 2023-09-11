0cf824ed4943b9746983032b8dad1848c35153f1
swiftleah <leahrosehenry@gmail.com>
2023-09-11 07:03:12 -0700

    Task 7 - added documentation under integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,16 +5,16 @@
 class BaseGeometry:
     """ define instances:"""
     def area(self):
-        """ public instance area
-        Args:
-            self
-        Raises:
-            ValueError
-            TypeError
+        """ public instance area - raises Exception
         """
         raise Exception("area() is not implmeneted")
 
     def integer_validator(self, name, value):
+        """
+        Integer-Validator - checks if given correct argument
+        Args: name & value
+        Int must be an int & cannot be <= 0, otherwise Exceptions are raised
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
