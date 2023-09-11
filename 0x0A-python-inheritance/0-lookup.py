b4357e60cc8156a2181894febc3d3ceca3c94d28
swiftleah <leahrosehenry@gmail.com>
2023-09-11 02:17:35 -0700

    Task 0 completed - used dir() function to return all methods & attributes of function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function def lookup: returns list of attributes and methods of object
+using dir()
+"""
+
+
+def lookup(obj):
+    """ Function lookup
+    Args: object
+    """
+    return dir(obj)
