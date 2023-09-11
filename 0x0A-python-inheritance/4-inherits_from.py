cb13eb3d6776fc863b25f034e0ad07281567346f
swiftleah <leahrosehenry@gmail.com>
2023-09-11 02:50:38 -0700

    Task 4 attempt 1

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+inherits_from - returns True of obj is instance of class that inherited
+from specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    define function
+    Args: obj, a_class
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
