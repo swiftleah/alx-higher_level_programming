ff36cdbba5cbd3fc33dda93a0edad94d1993bcfa
swiftleah <leahrosehenry@gmail.com>
2023-09-11 03:05:49 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ define area"""
+    def area(self):
+        """ area - raises exception
+        Args: self
+        """
+        raise Exception("area() is not implemented")
