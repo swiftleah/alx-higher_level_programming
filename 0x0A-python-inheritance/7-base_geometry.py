d47fe308d49a2333e804175ab70eea945ba9372e
swiftleah <leahrosehenry@gmail.com>
2023-09-11 03:12:27 -0700

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class BaseGeomtry with Exception messages
+"""
+
+
+class BaseGeometry:
+    """ define instances:"""
+    def area(self):
+        """ public instance area
+        Args: self
+        """
+        raise Exception("area() is not implmeneted")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
