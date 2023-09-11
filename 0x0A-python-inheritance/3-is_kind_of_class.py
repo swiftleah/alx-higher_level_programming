fd1f81f4e4627244c89230c6f8b33d9717b008ce
swiftleah <leahrosehenry@gmail.com>
2023-09-11 02:45:02 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function is_kind_of_class
+returns True f object is instance of a class that inherited from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ define function
+    Args: obj
+    a_class
+    """
+    return isinstance(obj, a_class)
