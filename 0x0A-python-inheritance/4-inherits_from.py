6b6b5bdee8fca6522e25fa333d372122b8910bc1
swiftleah <leahrosehenry@gmail.com>
2023-09-11 05:05:48 -0700

    Changed documentation task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-inherits_from - returns True of obj is instance of class that inherited
-from specified class
-"""
+"""inherits_from - returns True of obj is instance of class that inherited"""
 
 
 def inherits_from(obj, a_class):
     """
     define function
-    Args: obj, a_class
-    Returns: True if obj is instance of base class
+    Args:
+        obj, a_class
+    Returns:
+        True if obj is instance of base class
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
