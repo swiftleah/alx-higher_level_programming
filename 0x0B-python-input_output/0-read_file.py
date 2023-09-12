ffe6b67fe64f2150bc43949d7fcb3d831aa489ca
swiftleah <leahrosehenry@gmail.com>
2023-09-12 00:37:34 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """ Define function
     reads text in file & prints to stdout """
-    with open(filename, 'r', encoding='utf-8') as my_file:
+    with open(filename, encoding='utf-8') as my_file:
         my_file_contents = my_file.read()
 
     print(my_file_contents, end="")
