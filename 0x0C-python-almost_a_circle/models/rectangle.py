6613dd2f64ef345861ee59a8d476705addea2fe5
swiftleah <leahrosehenry@gmail.com>
2023-09-14 07:38:06 -0700

    Task 2 - changed initialization of arguments to 'self.width = width' instead of private attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,10 +12,10 @@ class Rectangle(Base):
         Args: width, height, x, y, id """
         super().__init__(id)
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
