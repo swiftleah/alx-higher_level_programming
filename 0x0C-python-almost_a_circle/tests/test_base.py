75962e0fe0ba4d5193773d203ded65f1ebbf66ba
swiftleah <leahrosehenry@gmail.com>
2023-09-17 23:27:01 -0700

    Added another test for two rectangles

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -19,9 +19,19 @@ class TestBase_to_json_string(unittest.TestCase):
         self.assertEqual(Base.to_json_string(None), "[]")
 
     def test_to_json_string_one_rectangle_dict(self):
-        rect1 = Rectangle(9, 6, 1, 7)
+        rect1 = Rectangle(9, 6, 1, 7, 2)
         rect1_dict = rect1.to_dictionary()
 
         rect1_json = rect1.to_json_string([rect1_dict])
 
         self.assertEqual(len(rect1_json), 52)
+
+    def test_to_json_string_two_rectangle_dict(self):
+        rect1 = Rectangle(9, 6, 1, 7, 2)
+        rect2 = Rectangle(12, 7, 3, 8, 4)
+        rect1_dict = rect1.to_dictionary()
+        rect2_dict = rect1.to_dictionary()
+
+        list_dicts = [rect1.to_json_string([rect1_dict]), rect1.to_json_string([rect2_dict])]
+
+        self.assertTrue(len(list_dicts), 106)
