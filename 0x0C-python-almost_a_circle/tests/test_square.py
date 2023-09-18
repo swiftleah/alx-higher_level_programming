404d27515c56b089acc314005b2c5db55a7aa232
swiftleah <leahrosehenry@gmail.com>
2023-09-18 01:28:09 -0700

    More tests for square

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -9,6 +9,23 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
+class TestSquare_update(unittest.TestCase):
+    """ Unittest for Task 12: assigns attributes
+    args is list of arguments
+    kwargs used when args is empty or None """
+    def test_update_square_one_arg(self):
+        sq1 = Square(6)
+        sq1.update(1)
+
+        self.assertEqual("[Square] (1) 0/0 - 6", str(sq1))
+
+    def test_update_square_two_args(self):
+        sq1 = Square(6)
+        sq1.update(1, 2, 3, 4)
+
+        self.assertEqual("[Square] (1) 3/4 - 2", str(sq1))
+
+
 class TestSquare_to_dictionary(unittest.TestCase):
     """ unittests for task 14: returns dictionary representation
     of specified shape """
@@ -21,3 +38,13 @@ class TestSquare_to_dictionary(unittest.TestCase):
         sq1 = Square(10, 2, 5, 6)
 
         self.assertTrue(isinstance(sq1.to_dictionary(), dict))
+
+    def test_to_dictionary_one_updated_square(self):
+        sq1 = Square(10, 2, 5, 6)
+        sq2 = Square(13, 5, 8, 9)
+        sq1_update = sq1.to_dictionary()
+
+        self.assertNotEqual(sq1_update, sq2)
+
+if __name__ == "__main__":
+    unittest.main()
