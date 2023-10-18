acc69be627284e05be267f292d003d34b33f5632
swiftleah <leahrosehenry@gmail.com>
2023-10-18 00:24:11 -0700

    Task 2 - create database & user if not exists and grants select privileges on user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creates database if not exists
+-- creates user and grants all privileges if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
