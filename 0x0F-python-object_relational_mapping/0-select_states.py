07fbf3632631c99c9663c03ae47826506799c8ed
swiftleah <leahrosehenry@gmail.com>
2023-11-16 01:52:05 -0800

    Task 0 - for now - can't connect to mysql server

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" lists all states from db hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    mycursor = db.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id ASC")
+    result = mycursor.fetchall()
+
+    for row in result:
+        print(row)
+
+        cursor.close()
+        db.close()
