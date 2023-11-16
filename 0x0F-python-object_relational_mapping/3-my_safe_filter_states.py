260aef06cc3e37f5bf571d8cd625ddfd5d2cdc78
swiftleah <leahrosehenry@gmail.com>
2023-11-16 07:14:21 -0800

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" lists all states from db hbtn_0e_0_usa where name matches argument """
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    mycursor = db.cursor()
+    name = sys.argv[4]
+    mycursor.execute("SELECT * FROM states WHERE name LIKE %s"
+                     "ORDER BY id ASC", (name, ))
+    result = mycursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    mycursor.close()
+    db.close()
