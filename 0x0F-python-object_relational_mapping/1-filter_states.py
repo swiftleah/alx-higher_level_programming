af66840b0941b9c159470b00ecd16a5b892c11d1
swiftleah <leahrosehenry@gmail.com>
2023-11-16 23:27:10 -0800

    Task 1 - fixed pycodestyle errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
 
     mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    mycursor.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                      ORDER BY states.id""")
     result = mycursor.fetchall()
 
     for row in result:
