6610ecb0d3b3ac847e6db56dfc16c601e833427e
swiftleah <leahrosehenry@gmail.com>
2023-12-01 01:17:58 -0800

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# takes URL, sends req and displays body of response in utf-8 - otherwise error
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            decoded_response = response.read().decode('utf-8')
+            print(decoded_response)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
