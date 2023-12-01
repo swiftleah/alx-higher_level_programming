1d306b3babc3ae5e949a5dbb61654208fbe6569a
swiftleah <leahrosehenry@gmail.com>
2023-11-30 23:23:29 -0800

    Task 0 - fetches url with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# fetches https://alx-intranet.hbtn.io/status using urllib
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {utf8_content}")
