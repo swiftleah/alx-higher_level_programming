a4d750c237bde0423b34c4631edf3f1a8543821f
swiftleah <leahrosehenry@gmail.com>
2023-12-01 01:52:51 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# takes a letter and sends POST req to http://0.0.0.0:5000/search_user
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+    response = requests.post(url, data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
