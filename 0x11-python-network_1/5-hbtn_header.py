3bc4783135c9afa31f0a8efc010857232d71b969
swiftleah <leahrosehenry@gmail.com>
2023-12-01 01:26:36 -0800

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# takes URL, sends req and displays X-request-Id in response header
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print("{}".format(req.headers.get("X-Request-Id")))
