0a94d7052f7e3190d4df2536444c9998405fc0ee
swiftleah <leahrosehenry@gmail.com>
2024-01-03 05:15:21 -0800

    Task 1 - semistandard approved

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const filepath = process.argv[2];
 const str = process.argv[3];
 
 fs.writeFile(filepath, str, 'utf8', function (error) {
-	if (error) {
-		console.log(error);
-	}
+  if (error) {
+    console.log(error);
+  }
 });
