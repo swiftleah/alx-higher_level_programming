75b38b24fb716b8eaa76fc96632a3df06ff183a6
swiftleah <leahrosehenry@gmail.com>
2024-01-03 05:13:45 -0800

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// writes a string to a file
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filepath, str, 'utf8', function (error) {
+	if (error) {
+		console.log(error);
+	}
+});
